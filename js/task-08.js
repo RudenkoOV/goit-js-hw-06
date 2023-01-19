aa90ce5366503d16112d06b57c1155f9be801639
RudenkoOV <kpaxx@ukr.net>
2023-01-19 12:25:47 +0200

    ДЗ-6в1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEls = document.querySelector('.login-form');
+console.log(formEls);
+formEls.addEventListener('submit', checkForm);
 
+function checkForm(event) {
+    event.preventDefault();
+    const elements = event.currentTarget;
+
+  if (elements.email.value === "" || elements.password.value === "") {
+    return console.log("Alert!");
+  }
+      const refs = {
+          email: elements.email.value,
+          password: elements.password.value
+    }
+    return console.log(`Значение поля email: ${refs.email}, password: ${refs.password}`);
+    event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const formBtn = document.querySelector('.change-color');
+const formSpan = document.querySelector('.color');
+const formBody = document.querySelector('body');
+formBtn.addEventListener('click', changeColor);
+
+function changeColor () {
+  formSpan.textContent = getRandomHexColor();
+  formBody.style.backgroundColor = formSpan.textContent;
+}
