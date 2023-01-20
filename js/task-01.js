a83ef92e9b3da116866aea3ad6bf2ca0bbbf5805
RudenkoOV <kpaxx@ukr.net>
2023-01-20 15:16:39 +0200

    ДЗ-6-в3

--- js/task-01.js
+++ js/task-01.js
@@ -14,25 +14,12 @@
 // }
 
 // displayEl();
-const body = document.body;
 
-const ulEl = body.firstElementChild.nextElementSibling;
-console.log(`Number of categories: ${ulEl.children.length}`);
 
-const liitemEl = ulEl.firstElementChild; 
-const liitemh2El = liitemEl.firstElementChild;
-console.log(`Category: ${ liitemh2El.textContent }`);
-const listEl = liitemEl.lastElementChild;
-console.log(`Elements: ${listEl.children.length}`);
+const categoryEls = document.body.firstElementChild.nextElementSibling.children;
+console.log(`Number of categories: ${categoryEls.length}`);
+for (const categoryEl of categoryEls) {
+    console.log(`Category: ${categoryEl.firstElementChild.textContent}`);
+    console.log(`Elements: ${categoryEl.lastElementChild.children.length}`);
+}
 
-const liitemSiblingEl = ulEl.firstElementChild.nextElementSibling;
-const liitemSiblingh2El = liitemSiblingEl.firstElementChild;
-console.log(`Category: ${liitemSiblingh2El.textContent}`);
-const listSiblingEl = liitemSiblingEl.lastElementChild;
-console.log(`Elements: ${listSiblingEl.children.length}`);
-
-const liitemLastEl = ulEl.lastElementChild; 
-const liitemLasth2El = liitemLastEl.firstElementChild;
-console.log(`Category: ${liitemLasth2El.textContent}`);
-const listLastEl = liitemLastEl.lastElementChild;
-console.log(`Elements: ${listLastEl.children.length}`);
